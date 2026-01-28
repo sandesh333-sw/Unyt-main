80f6a6409358e59e2a7772bfb569d9994956908c
Sandesh Pulami <sandeshpulami333@gmail.com>
2026-01-28 10:43:52 +0000

    fix: following dry principle

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import NavBar from "./(main)/navbar/page";
+import Footer from "./(main)/footer/page";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,13 +22,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
+        >
+          <NavBar />
+          <main className="flex-1">
+            {children}
+          </main>
+          <Footer />
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,19 +1,12 @@
-import { UserButton } from "@clerk/nextjs";
-import Image from "next/image";
-import NavBar from "./(main)/navbar/page";
 import HeroPage from "./(main)/hero/page";
 import Featured from "./(main)/featured/page";
-import Footer from "./(main)/footer/page";
+
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <NavBar />
-      <main className="flex-1">
-        <HeroPage />
-        <Featured />
-      </main>
-      <Footer />
-    </div>
+    <>
+    <HeroPage />
+    <Featured />
+    </>
   );
 }
