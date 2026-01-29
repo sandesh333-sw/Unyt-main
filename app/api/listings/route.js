5ffdfc80a1ba84bb5c4cca01c0a57268c0bf9aee
Sandesh Pulami <sandeshpulami333@gmail.com>
2026-01-29 21:42:37 +0000

    feat: create POST endpoint

--- app/api/listings/route.js
+++ app/api/listings/route.js
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
-import { auth } from "@clerk/nextjs/dist/types/server";
+import { auth } from "@clerk/nextjs/server";
 import connectDB from "@/app/lib/mongodb";
 import Listing from "@/app/models/Listing";
 
-//Get all listings
+// GET -  All listings
 export async function GET(request){
     try {
         await connectDB();
@@ -22,3 +22,58 @@ export async function GET(request){
         );
     }
 }
+
+// POST - Create new Listing
+export async function POST(request){
+    try {
+        const { userId } = await auth();
+        
+
+        if(!userId){
+            return NextResponse.json(
+                { success: false, error: 'Unauthorized'},
+                { status: 401 }
+            );
+        }
+        console.log('User id is',userId);
+
+        
+        await connectDB();
+
+        const body = await request.json();
+
+        const { title, description, location, country, price, imageUrl, imagePublicId } = body;
+
+        // Validate
+        if (!title || !description || !location || !country || !price){
+            return NextResponse.json(
+                { status: false, error: 'Missing required files'},
+                { status: 400 }
+            );
+        }
+
+        const newListing = await Listing.create({
+            title,
+            description,
+            location,
+            country,
+            price,
+            imageUrl,
+            imagePublicId,
+            owner: 123,
+        });
+
+        return NextResponse.json(
+            { success: true, data: newListing},
+            { status: 201 }
+        );
+    } catch (error) {
+        console.error('Error creating listing:', error);
+        return NextResponse.json(
+            { success: false, error: 'Failed to create listing'},
+            { status: 500}
+        );
+    }
+}
+
+
