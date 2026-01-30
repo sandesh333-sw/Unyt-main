a98aef0e9dfab5c436909060b64290c36d35ce3e
Sandesh Pulami <sandeshpulami333@gmail.com>
2026-01-30 20:58:10 +0000

    feat: add delete endpoint

--- app/api/listings/[id]/route.js
+++ app/api/listings/[id]/route.js
@@ -1,4 +1,5 @@
 import { NextResponse } from "next/server";
+import { auth } from '@clerk/nextjs/server';
 import connectDB from "@/app/lib/mongodb";
 import Listing from "@/app/models/Listing";
 
@@ -90,6 +91,54 @@ export async function PUT(request, { params }){
     console.error('Error updating listings', error);
     return NextResponse.json(
       { success: false, error: 'Failed to update listing'},
+      { status: 500 }
+    );
+  }
+}
+
+//DELETE: listing
+export async function DELETE(request, { params }){
+  try {
+    const { userId } = await auth();
+
+    if (!userId){
+      return NextResponse.json(
+        { success: false, error: 'Unauthorized'},
+        { status: 401 }
+      );
+    }
+
+    await connectDB();
+
+    const { id } = await params;
+    const listing = await Listing.findById(id);
+
+    if (!listing){
+      return NextResponse.json(
+        { success: true, error: 'Listing not found'},
+        { status: 404 }
+      );
+    }
+
+    // Check if user owns the listing
+    if (listing.owner != userId){
+      return NextResponse.json(
+        { success: false, error: 'Forbidden - You do not own the listing'},
+        { status: 403 }
+      );
+    }
+
+    await Listing.findByIdAndDelete(id);
+
+    return NextResponse.json(
+      { success: true, message: 'Listing deleted successfully'},
+      { status: 200}
+    );
+
+  } catch (error) {
+    console.error('Error deleting listings:', error);
+    return NextResponse.json(
+      { success: false, error: 'Failed to delete listing'},
       { status: 500 }
     );
   }
--- middleware.js
+++ middleware.js
@@ -1,31 +1,35 @@
-// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// const isPublicRoute = createRouteMatcher([
-//   "/sign-in(.*)",
-//   "/sign-up(.*)",
-//   "/",
-//   "/api/listings(.*)", // allow GET /api/listings AND /api/listings/:id
-// ]);
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/",
+  "/api/listings(.*)", // allow GET /api/listings AND /api/listings/:id
+]);
 
-// const isProtectedApiRoute = createRouteMatcher([
-//   "/api/listings(.*)", // protect POST/PUT/DELETE on all listings routes
-// ]);
+const isProtectedApiRoute = createRouteMatcher([
+  "/api/listings(.*)", // protect POST/PUT/DELETE on all listings routes
+]);
 
-// export default clerkMiddleware(async (auth, req) => {
-//   // Protect write operations on listings
-//   if (isProtectedApiRoute(req) && req.method !== "GET") {
-//     await auth.protect();
-//   }
+export default clerkMiddleware(async (auth, req) => {
+  // Protect write operations on listings
+  if (isProtectedApiRoute(req) && req.method !== "GET") {
+    await auth.protect();
+  }
 
-//   // Protect everything else that isn't public
-//   if (!isPublicRoute(req) && !isProtectedApiRoute(req)) {
-//     await auth.protect();
-//   }
-// });
+  // Protect everything else that isn't public
+  if (!isPublicRoute(req) && !isProtectedApiRoute(req)) {
+    await auth.protect();
+  }
+});
 
-// export const config = {
-//   matcher: [
-//     "/((?!_next|.*\\..*).*)",
-//     "/(api|trpc)(.*)",
-//   ],
-// };
+export const config = {
+  matcher: [
+    "/((?!_next|.*\\..*).*)",
+    "/(api|trpc)(.*)",
+  ],
+};
+
+
+
+
