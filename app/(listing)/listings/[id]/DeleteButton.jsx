fa6ce9c28806e0da513e23057c18a709e0cdc868
Sandesh Pulami <sandeshpulami333@gmail.com>
2026-01-31 16:06:30 +0000

    feat: create a delete button

---
+++ app/(listing)/listings/[id]/DeleteButton.jsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { useRouter } from "next/navigation"
+import { Trash2 } from "lucide-react"
+import toast from 'react-hot-toast'
+
+const DeleteButton = ({listingId}) => {
+  const router = useRouter();
+  
+  const handleDelete = async () => {
+    if (!confirm('Are you sure you want to delete this listing?')){
+      return
+    }
+
+     try {
+      const res = await fetch(`/api/listing/${listingId}`, {
+        method: 'DELETE',
+      })
+
+      const data = await res.json();
+
+      if (data.success){
+        toast.success('Listing deleted successfully')
+        router.push('/listings')
+        router.refresh()
+      } else {
+        toast.error(data.error || 'Failed to delete listing')
+      }
+
+     } catch (error) {
+      console.error('Error deleting listilng:', error)
+      toast.error('Failed to delete listing')
+     }
+  }
+
+
+  return (
+    <button 
+    onClick={handleDelete}
+    className='px-6 py-2 border border-red-600 rounded-md font-semibold hover:bg-red-50 transition-colors flex items-center gap-2'
+    >
+      <Trash2  className='h-4 w-4'/>
+    </button>
+  )
+}
+
+export default DeleteButton
--- app/(listing)/listings/[id]/page.jsx
+++ app/(listing)/listings/[id]/page.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { MapPin, PoundSterling, ArrowLeft } from 'lucide-react'
+import { auth } from '@clerk/nextjs/server'
+import DeleteButton from './DeleteButton'
 
 const ViewListingPage = () => {
   return (
